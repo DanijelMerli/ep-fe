4bd19450259e00bfa12a23a596b2f866d6750414
Danijel Merli <danijel.merli@gmail.com>
2020-11-09 23:44:53 +0100

    added redirect to login

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'app-login-form',
+  selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+    if (this.authService.loggedIn()) {
+      this.router.navigate(['/']);
+    }
+  }
 
-  ngOnInit(): void {}
-
-  login() {
-    this.authService.login(this.model).subscribe(
-      (data) => {
-        console.log(data);
+  logIn() {
+    this.authService.logIn(this.model).subscribe(
+      () => {
+        this.router.navigate(['/']);
       },
       () => {
         console.log('Failed to login');
       }
     );
-  }
-
-  loggedIn() {
-    return !!this.authService.userToken;
   }
 }
